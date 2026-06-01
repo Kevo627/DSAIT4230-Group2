896e340b1eeffb7ab29c9874dc6d5cfd807cbd05
Carolyn <c.alcaraz@student.utwente.nl>
2026-06-01 16:13:46 +0200

    basic measures functions (no measures that require sampling yet)

---
+++ src/metrics/metrics.py
@@ -0,0 +1,133 @@
+import math
+import string
+from collections import Counter
+import re
+from bert_score import score
+
+### VQA
+
+class VQAScorer:
+    
+    ## Normalization
+    
+    def __init__(self):
+        self.manual_map = {
+            "none": "0",
+            "zero": "0",
+            "one": "1",
+            "two": "2",
+            "three": "3",
+            "four": "4",
+            "five": "5",
+            "six": "6",
+            "seven": "7",
+            "eight": "8",
+            "nine": "9",
+            "ten": "10",
+        }
+
+        self.articles = {"a", "an", "the"}
+
+        self.period_strip = re.compile(r"(?!<=\d)(\.)(?!\d)")
+        self.comma_strip = re.compile(r"(\d)(\,)(\d)")
+
+        self.punct = [
+            ";", "/", "[", "]", '"', "{", "}",
+            "(", ")", "=", "+", "\\", "_", "-",
+            ">", "<", "@", "`", ",", "?", "!",
+        ]
+        
+    def process_punctuation(self, text: str) -> str:
+        out_text = text
+
+        for p in self.punct:
+            if (p + " " in text or " " + p in text) or re.search(self.comma_strip, text):
+                out_text = out_text.replace(p, "")
+            else:
+                out_text = out_text.replace(p, " ")
+
+        out_text = self.period_strip.sub("", out_text)
+        return out_text
+
+
+    def process_digit_article(self, text: str) -> str:
+        words = text.lower().split()
+        output = []
+
+        for word in words:
+            word = self.manual_map.get(word, word)
+
+            if word not in self.articles:
+                output.append(word)
+
+        return " ".join(output)
+
+
+    def normalize(self, text: str) -> str:
+        text = text.replace("\n", " ")
+        text = text.replace("\t", " ")
+        text = text.strip()
+
+        text = self.process_punctuation(text)
+        text = self.process_digit_article(text)
+
+        return text
+    
+    
+    ## Main equations
+    
+    def vqa_score(self, prediction: str, answers: list[str]) -> float:
+        prediction = self.normalize(prediction)
+        answers = [self.normalize(answer) for answer in answers]
+        
+        match = sum(prediction == answer for answer in answers)
+        
+        score = min(1.0, match/3.0)
+        
+        return score
+    
+    # In case we want to switch to a more exact measure
+    def exact_match(self, prediction: str, answers: list[str]) -> float:
+        prediction = self.normalize(prediction)
+        answers = [self.normalize(answer) for answer in answers]
+
+        return float(prediction in answers)
+
+### BERTScore
+
+def bert_score_comp(generated_questions: list[str], reference_questions: list[str],) -> dict:
+    
+    P, R, F1 = score(generated_questions, reference_questions, lang="en", verbose=False)
+    
+    return {
+        "precision": float(P.mean()), 
+        "recall": float(R.mean()),
+        "f1": float(F1.mean()),
+        }
+
+### Improvement over baseline
+
+def improvement_over_baseline(strategy_score: float, baseline_score: float) -> dict:
+    diff = strategy_score - baseline_score
+    
+    if baseline_score == 0:
+        diff_percent = None
+    else:
+        diff_percent = (diff / baseline_score) * 100
+
+    return {
+        "diff": diff,
+        "diff_percent": diff_percent,
+    }
+
+### Entropy reduction (STILL TO DO)
+
+
+
+### Mean conversation length
+
+def mean_convo_length(conversation_lengths: list[int]) -> float:
+    if len(conversation_lengths) == 0:
+        return 0.0
+
+    return sum(conversation_lengths) / len(conversation_lengths)
